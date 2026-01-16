3cd670b6c89f22a968f5abc5a1826e8e279a1724
Harshit Mani Tripathi <harshit@hudater.dev>
2026-01-17 03:25:49 +0530

    character  count done

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from stats import *
 
+
 def main():
     book_text_str = get_book_text("./books/frankenstein.txt")
     # print(book_text_str)
-    word_count= get_word_count(book_text_str)
-    print(f"Found {word_count} total words")
+    word_count = get_word_count(book_text_str)
+    # print(f"Found {word_count} total words")
+    char_count_dict = get_char_count(book_text_str)
+    print(char_count_dict)
+
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -2,5 +2,17 @@ def get_book_text(file_path):
     with open(file_path) as f:
         return f.read()
 
+
 def get_word_count(file_content):
     return len(file_content.split())
+
+
+def get_char_count(file_content):
+    split_chars = list(str.lower(file_content))
+    char_count_dict = dict()
+    for i in split_chars:
+        if i in char_count_dict.keys():
+            char_count_dict[i] = char_count_dict[i] + 1
+        else:
+            char_count_dict[i] = 1
+    return char_count_dict
