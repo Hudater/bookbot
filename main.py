8a5e40155ce1138229d68992e88c30662427d543
Harshit Mani Tripathi <harshit@hudater.dev>
2026-01-16 21:25:25 +0530

    refactor

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-def get_book_text(file_path):
-    with open(file_path) as f:
-        file_content = f.read()
-    return file_content
-
-def get_word_count(file_content):
-    word_count = len(file_content.split())
-    print(f"Found {word_count} total words")
-
+from stats import *
 
 def main():
     book_text_str = get_book_text("./books/frankenstein.txt")
     # print(book_text_str)
-    get_word_count(book_text_str)
+    word_count= get_word_count(book_text_str)
+    print(f"Found {word_count} total words")
 
-main()
+if __name__ == "__main__":
+    main()
