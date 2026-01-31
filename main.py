fb4cbfa720efde5d5e64bbae8d03850ddf585b6c
Harshit Mani Tripathi <harshit@hudater.dev>
2026-01-31 06:19:46 +0530

    book name is arg now; new books added; BOOKBOT DONE

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+import sys
 import stats
 
 
 def main():
-    book_text_str = stats.get_book_text("./books/frankenstein.txt")
-    word_count = stats.get_word_count(book_text_str)
-    char_count_dict = stats.get_char_count(book_text_str)
-    sorted_list = stats.get_sorted_list_of_dicts(char_count_dict)
-    stats.report_formatting(sorted_list, word_count)
+    if len(sys.argv) == 2:
+        book_text_str = stats.get_book_text(sys.argv[1])
+        word_count = stats.get_word_count(book_text_str)
+        char_count_dict = stats.get_char_count(book_text_str)
+        sorted_list = stats.get_sorted_list_of_dicts(char_count_dict)
+        stats.report_formatting(sorted_list, word_count)
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
 
 if __name__ == "__main__":
